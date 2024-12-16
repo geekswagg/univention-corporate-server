8b500417e8c7639367046ca4daeb71372ac69f0b
Felix Botner <botner@univention.de>
2024-12-16 10:09:07 +0100

    Issue univention/ucs#2533: appliance installation

--- test/utils/installation_test/appliance-vnc-setup.py
+++ test/utils/installation_test/appliance-vnc-setup.py
@@ -89,8 +89,7 @@ class UCSSetup(VNCInstallation):
             raise VNCDoException()
 
         self.screenshot('network-setup.png')
-        # self.click_on('Preferred')
-        self.type("\t" * 2)
+        self.click_on('Preferred')
         if self.args.role in {'admember', 'slave'}:
             self.type(self.args.dns)
             time.sleep(0.5)
