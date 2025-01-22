01af00354e28daa4276a4b79f6a13fa3e05733f2
Felix Botner <botner@univention.de>
2025-01-22 10:11:12 +0100

    Bug #57114: use official appcenter server

--- management/univention-appcenter/python/appcenter-dev/actions/local_appcenter.py
+++ management/univention-appcenter/python/appcenter-dev/actions/local_appcenter.py
@@ -96,8 +96,8 @@ class DevUseTestAppcenter(UniventionAppAction):
 
     def main(self, args):
         if args.revert:
-            appcenter_server = 'appcenter-test.software-univention.de'
-            ucr_save({'repository/app_center/server': appcenter_server, 'update/secure_apt': 'false', 'appcenter/index/verify': 'no'})
+            appcenter_server = 'appcenter.software-univention.de'
+            ucr_save({'repository/app_center/server': appcenter_server, 'update/secure_apt': 'true', 'appcenter/index/verify': 'yes'})
         else:
             ucr_save({'repository/app_center/server': args.appcenter_host, 'update/secure_apt': 'false', 'appcenter/index/verify': 'no'})
         update = get_action('update')
