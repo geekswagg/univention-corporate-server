geekswagg/univention-corporate-server

commit 4dc5e373230a7968227b065bd04e81b300d39b63

Bug #57482: add an SQL database for shared session storage
The OIDC backchannel logout can hit any of the UMCs if multiprocessing
is configured. This commit introduces a persistent storage for sessions
between UMC processes.

Currently only the sessionId and various relevant OIDC token claims are
saved. This makes it not possible to use sessions between multiple UMC
processes but rather just adds the ability for other UMC processes to
delete the sessions of others.

Note: currently, if backchannel logout request is handled by a process
that is not the one it originated from the automatic tab refresh on
logout does not work.

Also add a test that tests OIDC backchannel logout with multiprocessing
enabled.

univention/ucs#2371

management/univention-management-console/src/univention/management/console/session_db.py
@@ -1,0 +1,120 @@
+import os
+import time
+from contextlib import contextmanager
+
+from sqlalchemy import BigInteger, Column, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from .config import SQL_CONNECTION_ENV_VAR
+
+
+class DBDisabledException(Exception):
+    pass
+
+
+class DBRegistry:
+    __engine = None
+    __registry = None
+    __init = False
+    _enabled = False
+
+    @classmethod
+    def get(cls):
+        if not cls.__init:
+            cls.__create()
+
+        return cls.__registry()
+
+    @classmethod
+    def enabled(cls):
+        if not cls.__init:
+            cls.__create()
+
+        return cls._enabled
+
+    @classmethod
+    def __create(cls):
+        cls.__init = True
+        connection_uri = os.environ.get(SQL_CONNECTION_ENV_VAR, None)
+        if connection_uri is None:
+            return
+
+        engine = create_engine(connection_uri)
+        cls.__engine = engine
+        cls.__registry = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+        Base.metadata.create_all(cls.__engine)
+        cls._enabled = True
+
+
+Base = declarative_base()
+
+
+@contextmanager
+def get_session(auto_commit=True):
+    # type: (bool) -> Iterator[sqlalchemy.Session]
+    if not DBRegistry.enabled():
+        raise DBDisabledException
+
+    session = None
+    try:
+        session = DBRegistry.get()
+        yield session
+    finally:
+        if session is not None:
+            if auto_commit:
+                session.commit()
+
+
+class DBSession(Base):
+    __tablename__ = 'sessions'
+    session_id = Column(String, primary_key=True)
+    expire_time = Column(BigInteger)
+    oidc_sid = Column(String)
+    oidc_sub = Column(String)
+    oidc_iss = Column(String)
+
+    sessions = {}
+
+    def __repr__(self):
+        return f'<Session(session_id={self.session_id}, expire_time={self.expire_time}, oidc_sid={self.oidc_sid}, oidc_sub={self.oidc_sub}, oidc_iss={self.oidc_iss})>'
+
+    @classmethod
+    def get(cls, db_session, session_id):
+        return db_session.query(cls).filter(cls.session_id == session_id).first()
+
+    @classmethod
+    def delete(cls, db_session, session_id):
+        db_session.query(cls).filter(cls.session_id == session_id).delete()
+
+    @classmethod
+    def update(cls, db_session, session_id, umc_session):
+        expire_time = cls.calculate_session_end_time(umc_session)
+        db_session.query(cls).filter(cls.session_id == session_id).update({'expire_time': expire_time})
+
+    @classmethod
+    def create(cls, db_session, session_id, umc_session):
+        oidc_params = {}
+        if umc_session.oidc:
+            oidc_params['oidc_sid'] = umc_session.oidc.claims.get('sid')
+            oidc_params['oidc_sub'] = umc_session.oidc.claims.get('sub')
+            oidc_params['oidc_iss'] = umc_session.oidc.claims.get('iss')
+
+        db_session.add(cls(session_id=session_id, expire_time=cls.calculate_session_end_time(umc_session), **oidc_params))
+        db_session.commit()
+
+    @classmethod
+    def calculate_session_end_time(cls, umc_session):
+        session_valid_in_seconds = umc_session.session_end_time - time.monotonic()
+        real_session_end_time = time.time() + session_valid_in_seconds
+
+        return real_session_end_time
+
+    @classmethod
+    def get_by_oidc(cls, db_session, claims):
+        oidc_sessions_by_sid = db_session.query(cls).filter(cls.oidc_iss == claims.get('iss'), cls.oidc_sid == claims.get('sid')).first()
+        if oidc_sessions_by_sid:
+            yield oidc_sessions_by_sid
+        else:
+            oidc_sessions_by_sub = db_session.query(cls).filter(cls.oidc_iss == claims.get('iss'), cls.oidc_sub == claims.get('sub')).all()
+            yield from oidc_sessions_by_sub

management/univention-management-console/src/univention/management/console/session_dict.py
@@ -1,0 +1,80 @@
+from collections.abc import MutableMapping
+
+from sqlalchemy import exc
+
+from univention.management.console.session_db import DBDisabledException, DBSession, get_session
+
+from .log import CORE
+
+
+class SessionDict(MutableMapping):
+    sessions = {}
+
+    def __delitem__(self, session_id) -> None:
+        try:
+            with get_session() as db_session:
+                DBSession.delete(db_session, session_id)
+        except exc.DBAPIError as err:
+            CORE.error('Deleting the session from the database failed\n%s' % (err))
+        except DBDisabledException:
+            pass
+
+        del self.sessions[session_id]
+
+    def __setitem__(self, session_id, umc_session) -> None:
+        try:
+            with get_session() as db_session:
+                session = DBSession.get(db_session, session_id)
+                if session:
+                    DBSession.update(db_session, session_id, umc_session)
+                else:
+                    DBSession.create(db_session, session_id, umc_session)
+        except exc.DBAPIError as err:
+            CORE.error('Adding the session into the database failed\n%s' % (err))
+        except DBDisabledException:
+            pass
+
+        self.sessions[session_id] = umc_session
+
+    def __getitem__(self, session_id):
+        local_session = self.sessions[session_id]
+        try:
+            with get_session() as db_session:
+                session = DBSession.get(db_session, session_id)
+                if not session:
+                    del self.sessions[session_id]
+                    raise KeyError(session_id)
+        except exc.DBAPIError as err:
+            CORE.error('Getting the session from the database failed\n%s' % (err))
+        except DBDisabledException:
+            pass
+
+        return local_session
+
+    def __len__(self) -> int:
+        return len(self.sessions)
+
+    def __iter__(self):
+        return self.sessions.__iter__()
+
+    def get_oidc_sessions(self, logout_token_claims):
+        try:
+            with get_session() as db_session:
+                sessions = DBSession.get_by_oidc(db_session, logout_token_claims)
+                return sessions
+        except exc.DBAPIError as err:
+            CORE.error('Getting OIDC sessions from the database failed\n%s' % (err))
+        except DBDisabledException:
+            pass
+
+        return self.__get_oidc_sessions_fallback(logout_token_claims)
+
+    def __get_oidc_sessions_fallback(self, logout_token_claims):
+        sessions = [user for user in self.sessions.values() if user and user.oidc and user.oidc.id_token and logout_token_claims['iss'] == user.oidc.claims['iss']]
+        for user in sessions:
+            if user.oidc.claims['sid'] == logout_token_claims.get('sid'):
+                yield user
+                return
+        for user in sessions:
+            if user.oidc.claims['sub'] == logout_token_claims.get('sub'):
+                yield user

management/univention-ldap/conffiles/etc/ldap/slapd.conf.d/60univention-ldap-server_acl-master
@@ -78,6 +78,12 @@ if configRegistry.get('ldap/hostdn'):
     print('    by * +0 break')
     print('')
 
+print('access to filter="(&(univentionObjectType=settings/data)(cn=umc))" attrs=univentionData')
+print('    by group/univentionGroup/uniqueMember="cn=%s,cn=groups,%s" read' % (groups_default_domainadmins, ldap_base))
+print('    by set="user/objectClass & [univentionDomainController]" read')
+print('    by * none')
+print('')
+
 @!@
 
 access to attrs=univentionRadiusPassword
@@ -86,3 +92,4 @@ access to attrs=univentionRadiusPassword
     by set="user/univentionObjectType & [computers/domaincontroller_slave]" read
     by set="user/univentionObjectType & [computers/memberserver]" read
     by * none
+

management/univention-management-console/debian/control
@@ -89,6 +89,7 @@ Depends:
  python3-pysaml2,
  python3-sdnotify,
  python3-setproctitle,
+ python3-sqlalchemy,
  python3-tornado,
  python3-univention,
  python3-univention-config-registry,

management/univention-management-console/debian/univention-management-console-server.univention-config-registry-variables
@@ -19,6 +19,13 @@ Type=uint
 Categories=management-umc
 Default=64
 
+[umc/http/session/db-url]
+Description[de]=Diese Variable konfiguriert die URL der SQL-Verbindung für die Sitzungsdatenbank.
+Description[en]=This variable configures the SQL connection URL for the UMC session database.
+Type=str
+Categories=management-umc
+Default=sqlite://
+
 [umc/module/debug/level]
 Description[de]=Der Detailgrad der Logmeldungen in /var/log/univention/management-console-module-*. Mögliche Werte: 0-4/99 (0: nur Fehlermeldungen bis 4: alle Debugausgaben, mit 99 werden auch sensible Daten wie Klartext-Passwörter protokolliert).
 Description[en]=The verbosity of log messages in /var/log/univention/management-console-module-*. Possible values: 0-4/99 (0: only error messages to 4: all debug statements, with = 99 sensitive data like cleartext passwords is logged as well).

management/univention-management-console/scripts/univention-management-console-settings
@@ -1,0 +1,123 @@
+#!/usr/bin/python3
+#
+# Univention Management Console
+#
+# Like what you see? Join us!
+# https://www.univention.com/about-us/careers/vacancies/
+#
+# Copyright 2006-2024 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
+import argparse
+import json
+import sys
+
+from univention.config_registry import ucr
+from univention.udm import UDM
+from univention.udm.binary_props import Base64Bzip2BinaryProperty
+from univention.udm.exceptions import NoObject
+
+
+settings_position = f"cn=umc,cn=data,cn=univention,{ucr.get('ldap/base')}"
+
+
+class StoreInDict(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string) -> None:
+        if not hasattr(namespace, 'options'):
+            namespace.options = {}
+
+        options = namespace.options
+        options[self.dest] = values
+        namespace.options = options
+
+
+class UMCSettings:
+    def __init__(self, position: str = settings_position):
+        self.udm = UDM.admin().version(3)
+        self.module = self.udm.get('settings/data')
+        try:
+            settings_obj = self.module.get(settings_position)
+        except NoObject:
+            settings_obj = self.module.new()
+            settings_obj.props.name = 'umc'
+            settings_obj.props.data_type = 'string'
+        self.obj = settings_obj
+        self._tmp_settings = {}
+
+    def get_settings(self):
+        if self.obj.props.data:
+            return json.loads(self.obj.props.data.raw.decode('utf-8'))
+        return {}
+
+    def set_setting(self, key, value):
+        self._tmp_settings[key] = value
+
+    def save(self):
+        new_settings = {**self.get_settings(), **self._tmp_settings}
+        self.obj.props.data = Base64Bzip2BinaryProperty('data', raw_value=json.dumps(new_settings).encode('utf-8'))
+        self.obj.save()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='univention-management-console-settings',
+    )
+
+    subparser = parser.add_subparsers(title='commands', required=True, dest='command', help='print the current configuration or set options')
+    get_subparser = subparser.add_parser('get')
+    get_subparser.set_defaults(func=get_options)
+
+    set_subparser = subparser.add_parser('set')
+    set_subparser.add_argument('-u', '--sqlURI', nargs='?', const=None, help='The SQL connection URI', action=StoreInDict)
+    set_subparser.set_defaults(func=set_options)
+
+    args = parser.parse_args()
+    if 'options' not in args and args.command == 'set':
+        set_subparser.print_help()
+        sys.exit(1)
+    return args
+
+
+def main():
+    args = parse_args()
+    settings = UMCSettings()
+
+    args.func(args, settings)
+
+
+def get_options(args, settings: UMCSettings):
+    print(settings.get_settings())
+
+
+def set_options(args, settings: UMCSettings):
+    for key, value in args.options.items():
+        settings.set_setting(key, value)
+    settings.save()
+
+
+if __name__ == '__main__':
+    main()

management/univention-management-console/src/univention/management/console/config.py
@@ -60,3 +60,5 @@ MODULE_COMMAND = '/usr/sbin/univention-management-console-module'
 
 MODULE_DEBUG_LEVEL = ucr.get_int('umc/module/debug/level', 2)
 MODULE_INACTIVITY_TIMER = ucr.get_int('umc/module/timeout', 600) * 1000
+
+SQL_CONNECTION_ENV_VAR = 'UMC_SQL_CONNECTION_URI'

management/univention-management-console/src/univention/management/console/oidc.py
@@ -42,6 +42,7 @@ from urllib.parse import urlencode, urlparse, urlunsplit
 
 import jwt
 from jwt.algorithms import RSAAlgorithm
+from sqlalchemy import exc
 from tornado import escape
 from tornado.auth import OAuth2Mixin
 from tornado.httpclient import HTTPClientError, HTTPRequest
@@ -52,6 +53,7 @@ from univention.management.console.error import BadRequest, NotFound, OpenIDProv
 from univention.management.console.log import CORE
 from univention.management.console.resource import Resource
 from univention.management.console.session import Session
+from univention.management.console.session_db import DBDisabledException, get_session
 from univention.management.console.shared_memory import shared_memory
 
 
@@ -534,15 +536,17 @@ class OIDCBackchannelLogout(OIDCResource):
             self.finish({'error': 'invalid_request', 'error_description': str(exc)})
             return
 
-        sessions = [user for user in Session.sessions.values() if user and user.oidc and user.oidc.id_token and claims['iss'] == user.oidc.claims['iss']]
-        for user in sessions:
-            if user.oidc.claims['sid'] == claims.get('sid'):
-                user.logout()
-                break
-        else:
-            for user in sessions:
-                if user.oidc.claims['sub'] == claims.get('sub'):
-                    user.logout()
+        for session in Session.sessions.get_oidc_sessions(claims):
+            if session.session_id in Session.sessions.sessions:
+                Session.sessions[session.session_id].logout()
+            else:
+                try:
+                    with get_session() as db_session:
+                        session.delete(db_session, session.session_id)
+                except exc.DBAPIError as err:
+                    CORE.error('Deleting the from the database during OIDC backchannel logout failed\n%s' % (err))
+                except DBDisabledException:
+                    pass
 
         self.finish()
 

commit 007b88edda36f8230a2f178d2c569d703a35050a

docs: add missing copyright header
Fixes: 4dc5e373230a7968227b065bd04e81b300d39b63
Bug #57482

management/univention-management-console/src/univention/management/console/session_db.py
@@ -1,3 +1,35 @@
+#!/usr/bin/python3
+#
+# Univention Management Console
+#  UMC server
+#
+# Copyright 2024-2025 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
 import os
 import time
 from collections.abc import Generator

management/univention-management-console/src/univention/management/console/session_dict.py
@@ -1,3 +1,35 @@
+#!/usr/bin/python3
+#
+# Univention Management Console
+#  UMC server
+#
+# Copyright 2024-2025 Univention GmbH
+#
+# https://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <https://www.gnu.org/licenses/>.
+
 from collections.abc import MutableMapping
 
 from sqlalchemy import exc