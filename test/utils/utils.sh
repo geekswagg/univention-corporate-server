b23ea16100ac3a715777dc421c83cff324d7aea6
Dirk Wiesenthal <wiesenthal@univention.de>
2025-04-28 11:00:01 +0200

    Issue univention/ucs#2338: Migrate postgres from 11 to 15 before installing Keycloak. Just in case...

--- test/utils/utils.sh
+++ test/utils/utils.sh
@@ -146,6 +146,8 @@ rotate_logfiles () {
 }
 
 prepare_domain_for_ucs52_preup_checks() {
+	postgres_update '11' '15'
+
 	/usr/share/univention-directory-manager-tools/udm-remap-country-from-st-to-c || return $?
 
 	univention-ldapsearch -LLL '(&(objectClass=univentionNagiosServiceClass)(!(univentionNagiosUseNRPE=1)))' 1.1 | sed -rne 's#^dn: ##p' | while read -r dn; do udm nagios/service remove --dn "$dn"; done
