geekswagg/univention-corporate-server

commit a3f46b7d5b0469049ae14d5363084a69f71df5b8

feat(udm-umc): Scope search for 'All Containers' queries
Since in larger ucs systems with thousands of users in multiple ous
searches that search over the entirety of the ldap_base could take
extremely long. To prevent this a search as ouadmin will now limit the
search to only the containers the ouadmin has access to.

Bug #58418
Issue #2839

management/univention-management-console-module-udm/umc/python/udm/__init__.py
@@ -615,14 +615,23 @@ class Instance(Base, ProgressMixin, metaclass=UDMModuleMeta):
             request.options['container'] = module.ldap_base
 
         container = request.options.get('container')
+        containers = module.get_default_containers() if container == 'default' else [container]
+        # Reduce list of containers to avoid duplicate results
+        if len(containers) > 1:
+            containers = [
+                c1
+                for c1 in containers
+                if not any(c2 for c2 in containers if c1.endswith(c2) and c2 != c1)
+            ]
+
         objectProperty = request.options['objectProperty']
         objectPropertyValue = request.options['objectPropertyValue']
         scope = request.options.get('scope', 'sub')
         hidden = request.options.get('hidden')
         fields = (set(request.options.get('fields', []) or []) | {objectProperty}) - {'name', 'None'}
-        result = module.search(container, objectProperty, objectPropertyValue, superordinate, scope=scope, hidden=hidden, allow_asterisks=USE_ASTERISKS)
-        if result is None:
-            return []
+        result = []
+        for container in containers:
+            result.extend(module.search(container, objectProperty, objectPropertyValue, superordinate, scope=scope, hidden=hidden, allow_asterisks=USE_ASTERISKS))
 
         entries = []
         object_type = request.options.get('objectType', request.flavor)

base/univention-l10n-fr/fr/management/univention-management-console-module-udm/umc/js/fr.po
@@ -187,9 +187,13 @@ msgstr "Options avancées"
 msgid "Advanced settings"
 msgstr "Réglages avancés"
 
-#: umc/js/udm.js:904 umc/js/udm.js:921
+#: umc/js/udm.js:934
 msgid "All containers"
 msgstr "Tous les conteneurs"
+
+#: umc/js/udm.js:937
+msgid "Default containers"
+msgstr "Conteneurs par défaut"
 
 #: umc/js/udm.js:903 umc/js/udm.js:906 umc/js/udm.js:918 umc/js/udm.js:933
 msgid "All types"

management/univention-management-console-module-udm/debian/univention-management-console-module-udm.univention-config-registry-variables
@@ -45,3 +45,17 @@ Description[de]=Definiert, ob das referenzierte Widget im Benutzererstellungsass
 Description[en]=Defines if the referenced widget is shown in the user create wizard by default (placeholder are e.g. invite, overridePWLength or pwdChangeNextLogin).
 Type=bool
 Categories=management-umc
+
+[directory/manager/web/modules/search/global-search]
+Description[de]=Definiert, ob die Suche nach Objekten über alle Container (LDAP Basis) möglich ist (auch modulspezifisch über z.B. 'directory/manager/web/modules/users/user/search/global-search').
+Description[en]=Defines whether searching for objects across all containers (LDAP base) is possible (also module specific via e.g. directory/manager/web/modules/users/user/search/global-search).
+Type=bool
+Default=true
+Categories=management-umc
+
+[directory/manager/web/modules/search/default-search]
+Description[de]=Definiert, ob die Suche nach Objekten über alle Standard-Container möglich ist (auch modulspezifisch über z.B. 'directory/manager/web/modules/users/user/search/global-search').
+Description[en]=Defines whether searching for objects across all default containers is possible (also module specific via e.g. directory/manager/web/modules/users/user/search/global-search).
+Type=bool
+Default=false
+Categories=management-umc

management/univention-management-console-module-udm/umc/js/de.po
@@ -208,9 +208,13 @@ msgstr ""
 "Hier verwaltete Alarme werden von der Applikation \"%(name)s\" benutzt, "
 "welche momentan nicht installiert ist."
 
-#: umc/js/udm.js:938 umc/js/udm.js:955
+#: umc/js/udm.js:934
 msgid "All containers"
 msgstr "Alle Container"
+
+#: umc/js/udm.js:937
+msgid "Default containers"
+msgstr "Standard-Container"
 
 #: umc/js/udm.js:937 umc/js/udm.js:940 umc/js/udm.js:952 umc/js/udm.js:967
 msgid "All types"

management/univention-management-console-module-udm/umc/js/udm.js
@@ -897,6 +897,10 @@ define([
 			var _isAdvancedSearch = this._ucr['directory/manager/web/modules/' + this.moduleFlavor + '/search/advanced_on_open'] ||
 				this._ucr['directory/manager/web/modules/search/advanced_on_open'];
 			this._isAdvancedSearch = tools.isTrue(_isAdvancedSearch);
+			var _globalSearch = this._ucr['directory/manager/web/modules/' + this.moduleFlavor + '/search/global-search'] || this._ucr['directory/manager/web/modules/search/global-search'] || 'true';
+			this._globalSearch = tools.isTrue(_globalSearch);
+			var _defaultSearch = this._ucr['directory/manager/web/modules/' + this.moduleFlavor + '/search/default-search'] || this._ucr['directory/manager/web/modules/search/default-search'] || 'false';
+			this._defaultSearch = tools.isTrue(_defaultSearch)
 
 			var umcpCmd = lang.hitch(this, 'umcpCommand');
 			var widgets = [];
@@ -926,7 +930,12 @@ define([
 				objTypes.push({ id: this.moduleFlavor, label: _('All types') });
 			} else if (containers && containers.length) {
 				// containers...
-				containers.unshift({ id: 'all', label: _('All containers') });
+				if (this._globalSearch) {
+					containers.unshift({ id: 'all', label: _('All containers') });
+				}
+				if (this._defaultSearch) {
+					containers.unshift({ id: 'default', label: _('Default containers') })
+				}
 				widgets.push({
 					type: ComboBox,
 					name: 'container',

commit 502481940ecd3be347326d5e39ac55cb9b0195cb

fix(udm-umc): ignore unsucessfull search results
MODULE      ( ERROR   ) : Internal server error during "udm/query (users/user)".: Traceback (most recent call last):
  File "/usr/lib/python3/dist-packages/univention/management/console/modules/decorators.py", line 235, in _run
    result = self._function(*args, **kwargs)  # type: Union[BaseException, _T]
             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/lib/python3/dist-packages/univention/management/console/modules/udm/__init__.py", line 640, in query
    result.extend(module.search(container, objectProperty, objectPropertyValue, superordinate, scope=scope, hidden=hidden, allow_asterisks=USE_ASTERISKS))
TypeError: 'NoneType' object is not iterable

Fixes: a3f46b7d5b0469049ae14d5363084a69f71df5b8
Bug #58418

management/univention-management-console-module-udm/umc/python/udm/__init__.py
@@ -637,7 +637,7 @@ class Instance(Base, ProgressMixin, metaclass=UDMModuleMeta):
         fields = (set(request.options.get('fields', []) or []) | {objectProperty}) - {'name', 'None'}
         result = []
         for container in containers:
-            result.extend(module.search(container, objectProperty, objectPropertyValue, superordinate, scope=scope, hidden=hidden, allow_asterisks=USE_ASTERISKS))
+            result.extend(module.search(container, objectProperty, objectPropertyValue, superordinate, scope=scope, hidden=hidden, allow_asterisks=USE_ASTERISKS) or [])
 
         entries = []
         object_type = request.options.get('objectType', request.flavor)