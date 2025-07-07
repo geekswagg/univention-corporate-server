geekswagg/univention-corporate-server

commit a78614db7618ca803502ede4a01b8c146dc27d1e

fix(udm): use priviledged LDAP connection
Bug #58432

management/univention-management-console-module-udm/umc/python/udm/udm_ldap.py
@@ -286,7 +286,7 @@ class ObjectDoesNotExist(UMCError):
     @LDAP_Connection
     def _ldap_object_exists(self, ldap_connection=None, ldap_position=None):
         try:
-            ldap_connection.get(self.ldap_dn, required=True)
+            ldap_connection.authz_connection.get(self.ldap_dn, required=True)
         except NO_SUCH_OBJECT:
             return False
         else:
@@ -681,12 +681,13 @@ class UDM_Module:
             raise SearchLimitReached()
         except (LDAPError, udm_errors.ldapError):
             raise
+        except udm_errors.noObject as e:
+            if superordinate:  # and not ldap_connection.authz_connection.get(superordinate):  # TODO: information disclosure!
+                raise SuperordinateDoesNotExist(superordinate)
+            if container:  # and not ldap_connection.authz_connection.get(container):  # TODO: information disclosure!
+                raise ObjectDoesNotExist(container)
+            UDM_Error(e).reraise()
         except udm_errors.base as e:
-            if isinstance(e, udm_errors.noObject):
-                if superordinate and not ldap_connection.get(superordinate):
-                    raise SuperordinateDoesNotExist(superordinate)
-                if container and not ldap_connection.get(container):
-                    raise ObjectDoesNotExist(container)
             UDM_Error(e).reraise()
 
         # call the garbage collector manually as many parallel request may cause the
@@ -717,7 +718,7 @@ class UDM_Module:
             raise
         except udm_errors.base as exc:
             MODULE.info('Failed to retrieve LDAP object: %s' % (exc,))
-            if isinstance(exc, udm_errors.noObject) and superordinate and not ldap_connection.get(superordinate.dn):
+            if isinstance(exc, udm_errors.noObject) and superordinate and not ldap_connection.authz_connection.get(superordinate.dn):  # TODO: information disclosure!
                 raise SuperordinateDoesNotExist(superordinate)
             UDM_Error(exc).reraise()
 
@@ -1098,7 +1099,7 @@ class UDM_Module:
         references = []
         if self.is_policy_module():  # TODO: move into the handlers/policies/*.py
             search_filter = filter_format("(&(objectClass=univentionPolicyReference)(univentionPolicyReference=%s))", (dn,))
-            for dn in ldap_connection.searchDn(filter=search_filter):
+            for dn in ldap_connection.authz_connection.searchDn(filter=search_filter):  # TODO: filter
                 obj, module = get_obj_module(None, dn, ldap_connection)
                 if not module or not obj:
                     continue
@@ -1224,7 +1225,7 @@ def get_module(flavor, ldap_dn, ldap_connection=None, ldap_position=None):
 
 
 def get_obj_module(flavor, ldap_dn, ldap_connection=None, ldap_position=None):
-    attr = ldap_connection.get(ldap_dn, ['*', '+'])  # TODO: we should use module.object._ldap_attributes() here but we don't have the module yet
+    attr = ldap_connection.authz_connection.get(ldap_dn, ['*', '+'])  # TODO: we should use module.object._ldap_attributes() here but we don't have the module yet
     module = _get_module(flavor, ldap_dn, attr, ldap_connection, ldap_position)
     if module is None:
         return None, None
@@ -1257,7 +1258,7 @@ def _get_module(flavor, ldap_dn, attributes=None, ldap_connection=None, ldap_pos
 def list_objects(container, object_type=None, ldap_connection=None, ldap_position=None):
     """Yields UDM objects"""
     try:
-        result = ldap_connection.search(base=container, scope='one')
+        result = ldap_connection.authz_connection.search(base=container, scope='one')
     except (LDAPError, udm_errors.ldapError):
         raise
     except udm_errors.noObject:
@@ -1398,7 +1399,7 @@ def read_syntax_choices(syn, options=None, ldap_connection=None, ldap_position=N
     except udm_errors.base as e:
         if isinstance(e, udm_errors.noObject):
             container = options.get('base')
-            if container and not ldap_connection.get(container):
+            if container and not ldap_connection.authz_connection.get(container):
                 raise ObjectDoesNotExist(container)
         UDM_Error(e).reraise()
 

management/univention-directory-manager-modules/modules/univention/admin/__init__.py
@@ -79,7 +79,7 @@ def ucr_overwrite_properties(module: Any, lo: univention.admin.uldap.access) -> 
                             syntax = getattr(univention.admin.syntax, new_prop_val)
                             setattr(prop, attr, syntax())
                         else:
-                            if lo.searchDn(filter=filter_format(univention.admin.syntax.LDAP_Search.FILTER_PATTERN, [new_prop_val])):
+                            if lo.authz_connection.searchDn(filter=filter_format(univention.admin.syntax.LDAP_Search.FILTER_PATTERN, [new_prop_val])):
                                 syntax = univention.admin.syntax.LDAP_Search(new_prop_val)
                                 syntax._load(lo)
                                 setattr(prop, attr, syntax)

management/univention-directory-manager-modules/modules/univention/admin/allocators.py
@@ -74,7 +74,7 @@ def requestUserSid(
     algorithmical_rid_base = 1000
     rid = str(uid * 2 + algorithmical_rid_base)
 
-    searchResult = lo.search(filter='objectClass=sambaDomain', attr=['sambaSID'])
+    searchResult = lo.authz_connection.search(filter='objectClass=sambaDomain', attr=['sambaSID'])
     domainsid = searchResult[0][1]['sambaSID'][0].decode('ASCII')
     sid = domainsid + '-' + rid
 
@@ -96,7 +96,7 @@ def requestGroupSid(
     if generateDomainLocalSid:
         sid = 'S-1-5-32-' + rid
     else:
-        searchResult = lo.search(filter='objectClass=sambaDomain', attr=['sambaSID'])
+        searchResult = lo.authz_connection.search(filter='objectClass=sambaDomain', attr=['sambaSID'])
         domainsid = searchResult[0][1]['sambaSID'][0].decode('ASCII')
         sid = domainsid + '-' + rid
 
@@ -112,7 +112,7 @@ def acquireRange(
     scope='base',  # type: _Scopes
 ):  # type: (...) -> str
     log.debug('ALLOCATE: Start allocation for type = %r', atype)
-    start_id = lo.getAttr('cn=%s,cn=temporary,cn=univention,%s' % (ldap.dn.escape_dn_chars(atype), position.getBase()), 'univentionLastUsedValue')
+    start_id = lo.authz_connection.getAttr('cn=%s,cn=temporary,cn=univention,%s' % (ldap.dn.escape_dn_chars(atype), position.getBase()), 'univentionLastUsedValue')
 
     log.debug('ALLOCATE: Start ID = %r', start_id)
 
@@ -155,7 +155,7 @@ def acquireRange(
             else:
                 _filter = '(%s=%d)' % (attr, startID)
             log.debug('ALLOCATE: searchfor %r', _filter)
-            if lo.searchDn(base=position.getBase(), filter=_filter):
+            if lo.authz_connection.searchDn(base=position.getBase(), filter=_filter):
                 log.debug('ALLOCATE: Already used ID %r', startID)
                 univention.admin.locking.unlock(lo, position, atype, str(startID).encode('utf-8'), scope=scope)
                 if other:
@@ -184,16 +184,16 @@ def acquireUnique(
 
     if type == "aRecord":  # uniqueness is only relevant among hosts (one or more dns entries having the same aRecord as a host are allowed)
         univention.admin.locking.lock(lo, position, type, value.encode('utf-8'), scope=scope)
-        if not lo.searchDn(base=searchBase, filter=filter_format('(&(objectClass=univentionHost)(%s=%s))', (attr, value))):
+        if not lo.authz_connection.searchDn(base=searchBase, filter=filter_format('(&(objectClass=univentionHost)(%s=%s))', (attr, value))):
             return value
     elif type in ['groupName', 'uid'] and configRegistry.is_true('directory/manager/user_group/uniqueness', True):
         univention.admin.locking.lock(lo, position, type, value.encode('utf-8'), scope=scope)
-        if not lo.searchDn(base=searchBase, filter=filter_format('(|(&(cn=%s)(|(objectClass=univentionGroup)(objectClass=sambaGroupMapping)(objectClass=posixGroup)))(uid=%s))', (value, value))):
+        if not lo.authz_connection.searchDn(base=searchBase, filter=filter_format('(|(&(cn=%s)(|(objectClass=univentionGroup)(objectClass=sambaGroupMapping)(objectClass=posixGroup)))(uid=%s))', (value, value))):
             log.debug('ALLOCATE return %s', value)
             return value
     elif type == "groupName":  # search filter is more complex then in general case
         univention.admin.locking.lock(lo, position, type, value.encode('utf-8'), scope=scope)
-        if not lo.searchDn(base=searchBase, filter=filter_format('(&(%s=%s)(|(objectClass=univentionGroup)(objectClass=sambaGroupMapping)(objectClass=posixGroup)))', (attr, value))):
+        if not lo.authz_connection.searchDn(base=searchBase, filter=filter_format('(&(%s=%s)(|(objectClass=univentionGroup)(objectClass=sambaGroupMapping)(objectClass=posixGroup)))', (attr, value))):
             log.debug('ALLOCATE return %s', value)
             return value
     elif type == 'cn-uid-position':
@@ -205,14 +205,14 @@ def acquireUnique(
             return value
 
         assert base is not None
-        if all(ldap.dn.str2dn(x)[0][0][0] not in attrs for x in lo.searchDn(base=base, filter='(|%s)' % ''.join(filter_format('(%s=%s)', (attr, value)) for attr in attrs), scope=scope)):
+        if all(ldap.dn.str2dn(x)[0][0][0] not in attrs for x in lo.authz_connection.searchDn(base=base, filter='(|%s)' % ''.join(filter_format('(%s=%s)', (attr, value)) for attr in attrs), scope=scope)):
             return value
         raise univention.admin.uexceptions.alreadyUsedInSubtree('name=%r position=%r' % (value, base))
     elif type in ('mailPrimaryAddress', 'mailAlternativeAddress') and configRegistry.is_true('directory/manager/mail-address/uniqueness'):
         log.debug('LOCK univention.admin.locking.lock scope = %s', scope)
         univention.admin.locking.lock(lo, position, 'mailPrimaryAddress', value.encode('utf-8'), scope=scope)
         other = 'mailPrimaryAddress' if type == 'mailAlternativeAddress' else 'mailAlternativeAddress'
-        if not lo.searchDn(base=searchBase, filter=filter_format('(|(%s=%s)(%s=%s))', (attr, value, other, value))):
+        if not lo.authz_connection.searchDn(base=searchBase, filter=filter_format('(|(%s=%s)(%s=%s))', (attr, value, other, value))):
             log.debug('ALLOCATE return %s', value)
             return value
     elif type == 'mailAlternativeAddress':
@@ -220,7 +220,7 @@ def acquireUnique(
     else:
         log.debug('LOCK univention.admin.locking.lock scope = %s', scope)
         univention.admin.locking.lock(lo, position, type, value.encode('utf-8'), scope=scope)
-        if not lo.searchDn(base=searchBase, filter=filter_format('%s=%s', (attr, value))):
+        if not lo.authz_connection.searchDn(base=searchBase, filter=filter_format('%s=%s', (attr, value))):
             log.debug('ALLOCATE return %s', value)
             return value
 
@@ -267,7 +267,7 @@ def confirm(
     updateLastUsedValue=True,  # type: bool
 ):  # type: (...) -> None
     if type in ('uidNumber', 'gidNumber') and updateLastUsedValue:
-        lo.modify('cn=%s,cn=temporary,cn=univention,%s' % (ldap.dn.escape_dn_chars(type), position.getBase()), [('univentionLastUsedValue', b'1', value.encode('utf-8'))])
+        lo.authz_connection.modify('cn=%s,cn=temporary,cn=univention,%s' % (ldap.dn.escape_dn_chars(type), position.getBase()), [('univentionLastUsedValue', b'1', value.encode('utf-8'))])
     elif type == 'cn-uid-position':
         return
     univention.admin.locking.unlock(lo, position, type, value.encode('utf-8'), _type2scope[type])

management/univention-directory-manager-modules/modules/univention/admin/authorization.py
@@ -317,7 +317,7 @@ def get_user_roles(lo, user_dn: str) -> None:
     # FIXME: Why doesn't this allow at least "=" and "," at least in "context_name"?
     # Basically it should allow everything valid in an LDAP DN!? I.e.  case insensitive UTF-8 see https://ldapwiki.com/wiki/Wiki.jsp?page=Distinguished%20Name%20Case%20Sensitivity and https://ldapwiki.com/wiki/Wiki.jsp?page=Ou
 
-    data = lo.get(user_dn, attr=['univentionObjectType'])
+    data = lo.authz_connection.get(user_dn, attr=['univentionObjectType'])
     mod = univention.admin.modules.get(data['univentionObjectType'][0].decode('UTF-8'))
     obj = mod.object(None, lo, None, user_dn)
     obj.open()

management/univention-directory-manager-modules/modules/univention/admin/config.py
@@ -96,7 +96,7 @@ def getDefaultValue(lo, name, position=None):
         current_search_dn = parent_dn
         while current_search_dn:
             try:
-                res_list = lo.search(
+                res_list = lo.authz_connection.search(
                     filter='(|(objectClass=univentionDefault)(objectClass=univentionContainerDefault))',
                     base=current_search_dn,
                     attr=[att],
@@ -124,7 +124,7 @@ def getDefaultValue(lo, name, position=None):
 
     try:
         fallback_base = 'cn=univention,%s' % (lo.base,)
-        res_list_fallback = lo.search(
+        res_list_fallback = lo.authz_connection.search(
             filter='(|(objectClass=univentionDefault)(objectClass=univentionContainerDefault))',
             attr=[att],
             base=fallback_base,

management/univention-directory-manager-modules/modules/univention/admin/guardian_roles.py
@@ -89,7 +89,7 @@ def role_layout():
 
 @univention.admin._ldap_cache(ttl=60)
 def get_group_role(lo: univention.admin.uldap.access, dn: str) -> list[str]:
-    res = lo.get(dn, attr=['univentionGuardianMemberRoles'])
+    res = lo.authz_connection.get(dn, attr=['univentionGuardianMemberRoles'])
     return [x.decode('UTF-8') for x in res.get('univentionGuardianMemberRoles', [])]
 
 

management/univention-directory-manager-modules/modules/univention/admin/handlers/computers/__base.py
@@ -68,7 +68,7 @@ class ComputerObject(univention.admin.handlers.simpleComputer, nagios.Support, P
                 primaryGroupNumber = self.oldattr.get('gidNumber', [b''])[0].decode('ASCII')
                 log.debug('primary group number = %s', primaryGroupNumber)
                 if primaryGroupNumber:
-                    primaryGroupResult = self.lo.searchDn(filter_format('(&(objectClass=posixGroup)(gidNumber=%s))', [primaryGroupNumber]))
+                    primaryGroupResult = self.lo.authz_connection.searchDn(filter_format('(&(objectClass=posixGroup)(gidNumber=%s))', [primaryGroupNumber]))
                     if primaryGroupResult:
                         self['primaryGroup'] = primaryGroupResult[0]
                         log.debug('Set primary group = %s', self['primaryGroup'])

commit 9f87a2b49e49863bec842c0263ae56009b07a111

fix(udm-umc): fix error handling in case the LDAP base is not readable
e.g. users/ldap accounts.
Test-Case: 60_umc/32_udm_nonposix_user_actions

Fixes: a78614db7618ca803502ede4a01b8c146dc27d1e

Bug #58432

management/univention-management-console-module-udm/umc/python/udm/udm_ldap.py
@@ -681,12 +681,11 @@ class UDM_Module:
             raise SearchLimitReached()
         except (LDAPError, udm_errors.ldapError):
             raise
-        except udm_errors.noObject as e:
+        except udm_errors.noObject:
             if superordinate:  # and not ldap_connection.authz_connection.get(superordinate):  # TODO: information disclosure!
                 raise SuperordinateDoesNotExist(superordinate)
-            if container:  # and not ldap_connection.authz_connection.get(container):  # TODO: information disclosure!
-                raise ObjectDoesNotExist(container)
-            UDM_Error(e).reraise()
+            # if container and not ldap_connection.authz_connection.get(container):  # TODO: information disclosure!
+            raise ObjectDoesNotExist(container or ucr['ldap/base'])
         except udm_errors.base as e:
             UDM_Error(e).reraise()
 